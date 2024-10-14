5d5c24051b1bb8c83412ee9cd2c4c63524230d17
Lucas Panzera <lucaspanzera614@gmail.com>
2024-10-14 12:24:37 -0300

    Grafiti con todas las funcionalidades

--- Grafiti/encoderManual.ino
+++ Grafiti/encoderManual.ino
@@ -3,20 +3,19 @@
 #include "MPU6050.h"
 #include <Adafruit_NeoPixel.h>
 
-String colores[] = {"#000000", "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF"};
+String colores[] = {"#010101", "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF"};
 
 BleMouse bleMouse;
 MPU6050 accelgyro;
 
- int counter = 0; // Contador
- char direction = ' '; // Direccion
+int counter = 0; // Contador
+char direction = ' '; // Direccion
+long valorRel=0;
 
-
-const int sensor = 34;
 const int buttonPin = 25; 
 
-#define PIN 2 
-#define NUMPIXELS 16
+#define PIN 19 
+#define NUMPIXELS 19
 
 #define CLK 27
 #define DT 26
@@ -34,6 +33,20 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 unsigned long lastButtonPressTime = 0;
 const unsigned long debounceDelay = 200; 
 
+bool buttonHeld = false;
+
+
+void hexToRGB(String hexString, uint8_t &r, uint8_t &g, uint8_t &b) {
+  if (hexString.startsWith("#")) {
+    hexString = hexString.substring(1); // Remover el "#"
+  }
+  long number = (long) strtol(hexString.c_str(), NULL, 16); // Convertir a long
+  r = (number >> 16) & 0xFF; // Extraer el valor rojo
+  g = (number >> 8) & 0xFF;  // Extraer el valor verde
+  b = number & 0xFF;         // Extraer el valor azul
+}
+
+
 void setup() {
     Serial.begin(115200);
     pinMode(CLK, INPUT);
@@ -45,7 +58,7 @@ void setup() {
     
     // Llamar a updateEncoder() cuando un high o un low haya cambiado
     attachInterrupt(digitalPinToInterrupt(CLK), updateEncoder, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, FALLING);
+
 
 
     Wire.begin();
@@ -60,7 +73,7 @@ void setup() {
     Serial.println("Testing device connections...");
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
-    accelgyro.setXGyroOffset(0);
+    accelgyro.setXGyroOffset(100);
     accelgyro.setYGyroOffset(66);
     accelgyro.setZGyroOffset(65);
 
@@ -70,8 +83,12 @@ void setup() {
 
 int x;
 int y;
+uint8_t red, green, blue;
+const int smoothingFactor = 1.2; // Factor de suavizado
+int16_t gx, gy, gz;
 
 void loop() {
+    
     if (updateNeeded) {
         updateNeeded = false; // Resetear el flag
         Serial.print("Direction: ");
@@ -80,20 +97,55 @@ void loop() {
         Serial.println(counter);
     }
 
+    
     if (bleMouse.isConnected()) {
-        int16_t gx, gy, gz;
-        accelgyro.getRotation(&gx, &gy, &gz);
 
-        x = gz / 256;
-        y = gx / 256;    
+    accelgyro.getRotation(&gx, &gy, &gz);
+
+    // Suavizar las lecturas
+    x = (gz / 256) / smoothingFactor;
+    y = (gx / 256) / smoothingFactor;
+
+
+    if (x > 0) {
+      x = x / 1.05;  // Aplica una suavización adicional para movimientos a la izquierda
     }
+
+    Serial.print(x);
+    Serial.print("----");
+    Serial.println(y);
+
     bleMouse.move(-x, -y);
-    delay(1);
+  }
+     
+ 
+
+    hexToRGB(colores[counter], red, green, blue); // Convertir el color
+
+    for(int i=0; i<NUMPIXELS; i++) { 
+      pixels.setPixelColor(i, pixels.Color(red, green, blue));
+    }
+    pixels.show();
+
+
+    // Mantener el click si el botón está presionado
+    if (buttonHeld) {
+      bleMouse.press(MOUSE_LEFT);
+    } else {
+      bleMouse.release(MOUSE_LEFT);
+    }
+
+  if (digitalRead(buttonPin) == LOW) {
+        buttonHeld = true;  // El botón está presionado
+    } else {
+        buttonHeld = false; // El botón está liberado
+    }
+
+  delay(10);  
 }
 
 
 int current=1;
-int valorRel = 0;
 void updateEncoder() {
     int currentStateCLK = digitalRead(CLK);
     int currentStateDT = digitalRead(DT);
@@ -101,15 +153,17 @@ void updateEncoder() {
     // Comprobar si el estado del DT es diferente al estado del CLK
     if (currentStateCLK != lastStateCLK) {
         if (currentStateDT != currentStateCLK) {
-            counter = (counter == 7) ? 0 : counter + 1; // CW
+            counter++;
             direction = 'C';
             bleMouse.move(0,0,1);
         } else {
-            counter = (counter == 0) ? 7 : counter - 1; // CCW
+            counter--;
             direction = 'W';
             bleMouse.move(0,0,-1);
         }
         updateNeeded = true; // Indica que hay una actualización
+        if (counter>6) counter=0;
+        if (counter<0) counter=6;
     }
     
     lastStateCLK = currentStateCLK; // Guardar el último estado del CLK 
@@ -117,12 +171,10 @@ void updateEncoder() {
 }
 
 void buttonPressed() {
-    unsigned long currentTime = millis();
-    
-    // Comprobar si el tiempo desde la última presión es mayor que el debounceDelay
-    if (currentTime - lastButtonPressTime > debounceDelay) {
-      bleMouse.click(MOUSE_LEFT);
-      Serial.println("Button Pressed!"); // Imprimir mensaje al presionar el botón
-      lastButtonPressTime = currentTime; // Actualizar el tiempo del último pulso
+    // Cambiar el estado de buttonHeld basado en el estado del botón
+    if (digitalRead(buttonPin) == LOW) {
+        buttonHeld = true;  // El botón está presionado
+    } else {
+        buttonHeld = false; // El botón está liberado
     }
 }
